348d1dd6ea33ad98976f2fc93877a90f5fb8ebd6
md-siam-islam <mdsiamislam663@gmail.com>
2024-09-23 01:59:24 +0600

    Donation and History button js function added done

---
+++ home.js
@@ -0,0 +1,13 @@
+function getElementByIdText (id){
+  let elementText =  document.getElementById(id);
+  return elementText ;
+}
+
+
+function getInputFieldValueById(id){
+    let inputValue = document.getElementById(id).value ;
+    return inputValue ;
+}
+
+
+
--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello siam");
+document.getElementById("history").addEventListener("click", function () {
+//   console.log("hello");
+    let eleMent = getElementByIdText('main-container').classList.add('hidden') ;
+
+    let donationBtn = getElementByIdText('donation').classList.remove('bg-color1');
+
+    getElementByIdText('history').classList.add('bg-color1');
+
+});
+document.getElementById("donation").addEventListener("click", function () {
+
+    let eleMent = getElementByIdText('main-container').classList.remove('hidden') ;
+
+    let donationBtn = getElementByIdText('history').classList.remove('bg-color1');
+
+    getElementByIdText('donation').classList.add('bg-color1');
+
+});
+
+
+
